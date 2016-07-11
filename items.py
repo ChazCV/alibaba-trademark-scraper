189815857e627a455bddea368d2b5ef274c61324
ChazCV <chazvollmer@gmail.com>
2016-07-11 15:48:06 -0600

    Update items.py

--- items.py
+++ items.py
@@ -6,23 +6,23 @@ class AliItem(scrapy.Item):
 	   are used for storing the scraped data.
 	"""
 	
-	#Trademark searched.
+    #Trademark searched.
     brand = scrapy.Field()
 	
-	#Number of alibaba.com listings returned.
+    #Number of alibaba.com listings returned.
     hits = scrapy.Field()
 	
-	#Yes if brand appears in the title of the first listing.
+    #Yes if brand appears in the title of the first listing.
     in_title = scrapy.Field()
 	
-	#Category identification numbers for the first two related categories.
+    #Category identification numbers for the first two related categories.
     rel_cat_1 = scrapy.Field()
     rel_cat_2 = scrapy.Field()
-    
-	#Number of listings for the first two related categories.
-	rel_hits_1 = scrapy.Field()
+
+    #Number of listings for the first two related categories.
+    rel_hits_1 = scrapy.Field()
     rel_hits_2 = scrapy.Field()
 
-	#Yes if brand appears in the title of the related listings.
+    #Yes if brand appears in the title of the related listings.
     rel_in_title_1 = scrapy.Field()
     rel_in_title_2 = scrapy.Field()
